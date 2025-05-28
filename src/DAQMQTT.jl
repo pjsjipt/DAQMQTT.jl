45a165a9b0a5c6062379e41cda162735c14c5ff5
= <pjabardo@ipt.br>
2025-05-28 12:13:27 -0300

    Working on the interface
    I will try to do something very simple and figure out the interface as I go...

--- src/DAQMQTT.jl
+++ src/DAQMQTT.jl
@@ -1,14 +1,35 @@
 module DAQMQTT
+
+using MQTTClient
+
 using DAQCore
 using Dates
 
-mutable struct MQTTTopic{T,User} <: AbstractInputDev
+mutable struct MQTTTopic{T,U} <: AbstractInputDev
     devname::String
     broker::Tuple{String,UInt16}
-    user::User
+    user::U
     topic::String
     buffer::Vector{Tuple{DateTime,T}}
+    time::Float64
     subscribed::Bool
 end
 
+function DAQCore.daqconfig(dev::MQTT, time)
+    time < 0 &&  error("Time should be a positive number of seconds!")
+    time > 10_000 && error("Time is too large. In this framework it should be a couple of minute...")
+    
+    dev.time = time
+end
+
+function DAQCore.daqstart(dev::MQTT)
+    empty!(dev.buffer())
+    
+end
+
+
+function DAQCore.daqread(dev::MQTT)
+end
+
+function 
 end
